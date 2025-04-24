a60fcd88c92dbd54207b2fb726df954e40068a72
FadySabry712 <fadysabry95@yahoo.com>
2025-04-24 14:19:32 +0000

    task 3

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" type annotated function that returns the floor of an int """
+
+import math
+
+
+def floor(n: int) -> int:
+    """ returns the floor of an int """
+    return (math.floor(n))
