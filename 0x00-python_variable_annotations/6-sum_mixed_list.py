952e6d1bc4b7a3e09e3bbf3672746ab133a6bda9
FadySabry712 <fadysabry95@yahoo.com>
2025-04-24 14:38:12 +0000

    task 7

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ type annotatd function to retun sum of union of types """
-from typing import List
+from typing import Union, List
 
 
-def sum_mixed_list(mxd_lst: List[int | str]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """ return float sum of the mixed values """
     return float(sum(mxd_lst))
