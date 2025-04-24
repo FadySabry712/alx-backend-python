36d19e21b101b83520877b0ffa2b28e58bff512b
FadySabry712 <fadysabry95@yahoo.com>
2025-04-24 14:15:38 +0000

    task 2

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Basic type annotated function that returns type string """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Return concatenated strings """
+    return (str1 + str2)
