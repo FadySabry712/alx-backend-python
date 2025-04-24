59cc8653164c7a3a782a441e024c4433ad8034e1
FadySabry712 <fadysabry95@yahoo.com>
2025-04-24 14:22:39 +0000

    task 4

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Type annotated function to return str repr of an int """
+
+
+def to_str(n: float) -> str:
+    """ return str represenatino of n """
+    return (str(n))
