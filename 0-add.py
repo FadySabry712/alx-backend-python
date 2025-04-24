c6db09a53b76c683e96f411f8c1dcef3f7ee02e5
FadySabry712 <fadysabry95@yahoo.com>
2025-04-24 14:09:36 +0000

    task 0

---
+++ 0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" practicing type annotated functions """
+
+
+def add(a: float, b: float) -> float:
+    """ takes type float and returns type float """
+    return (a + b)
